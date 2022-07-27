e76eda2f70149c9ad367960ece6a1b636282c149
Cleverton  Santos <99937154+ClevertonCodev@users.noreply.github.com>
2022-07-26 21:00:38 -0300

    php

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>Sobre Nós</h4>
+  	 			<ul>
+  	 				<li><a href="#">nossos serviços</a></li>
+  	 				<li><a href="#">Planos e covêncios</a></li>
+  	 				<li><a href="#">politica de atendimento</a></li>
+  	 				<li><a href="#">outros serviçoes</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Endereço</h4>
+  	 			<ul>
+  	 				
+  	 				<li><a href="#">Av.Engenheiro Roberto Freire - SEAWAY - SHOPPING Loja: 26</a></li>
+  	 				<li><a href="#">Nosso número: +55 (84) 0000-0000</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>Modalidades de atendimento</h4>
+				   <ul>
+					   <li><a href="#">Domiciliar: Fisioterapia no conforto do seu lar!</a></li>
+					   <li><a href="#">Online: Está com covid? Temos a teleconsulta!</a></li>
+				   </ul>
+				   
+  	 			<div>
+					<ul class="consulta">
+						
+							 <li class="con"><a style="color:white;" href="https://api.whatsapp.com/send?phone=5584994105215"  target="_blank">AGENDE SUA CONSULTA</a></li>
+					</ul>
+						
+					</div>
+  	 		</div>
+			   <br>
+  	 		<div id="social-links" class="footer-col">
+  	 			<h4>Siga nossas redes sociais</h4>
+  	 				<ul class="social-links">
+					   
+						<a href="https://www.facebook.com/search/top?q=cleverton%20santos%20-%20fisioterapeuta"target="_blank"><i class="fab fa-facebook-f"></i></a>
+
+						<a href="https://www.instagram.com/fisiotres.fisioespecializada/"target="_blank"><i class="fab fa-instagram"></i></a>
+
+						<a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+
+						<a href="https://api.whatsapp.com/send?phone=5584994105215"><i class="fab fa-whatsapp"></i></a>
+
+						<a href="mailto:clevertonsantoscodev@gmail.com"><i class="fa fa-envelope"></i></a>
+
+						<a href="https://br.linkedin.com/?trk=BR-SEM_google-adwords_Jordan-brand-sign-up&mcid=6821526239111716925&trk2=ga_campid=12619604099_asid=122510712920_crid=509739556238_kw=linkedin%20site_d=c_tid=kwd-327301955753_n=g_mt=e_geo=9101480_slid=&gclid=Cj0KCQiAjc2QBhDgARIsAMc3SqRYomK3xW3eawGWk4fE4D61EDsbh_H4VW0Ck5YL445cjZfGA6M3zLgaAjIEEALw_wcB&gclsrc=aw.ds"><i class="fab fa-linkedin-in"></i></a>
+					   
+				   </ul>
+  	 		</div>
+			   
+			   
+  	 </div>
+  </footer>
+  <!-- Copyrights Section -->
+ <div class="copyright">&copy;2022 - <strong>FISIOTRÊS</strong></div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+
+    <title>Fisiotres</title>
+  
+  <link rel="shortcut icon" href="img/fisiotres.ico" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link rel="stylesheet" href="css/Estrutura.css">
+  <link rel="stylesheet" href="css/bootstrap-4.4.1.css">	
+ 
+  
+  
+</head>
+<body class=" pg1">
+	<header id="header">
+	  <a id="logo" href ="inicial.html" ><img src="img/pequeno.png" alt="FISIOTRÊS"></a >
+	  <nav id="nav">
+		<button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false"> <a href=""> </a>
+		  <span id="hamburger"></span>
+		</button>
+		<ul id="menu" role="menu">
+		  <li><a href="inicial.html">Início</a></li>
+		  <li><a href="pagina2.html">Nosso Trabalho</a></li>
+		  <li><a href="pagina3.html">Equipe</a></li>
+		  <li><a href="pagina4.html">Fale conosco</a></li>
+		  <li><a href="pagina5.html"> Fazer Login</a></li>
+		</ul>
+	  </nav>
+	</header>
+	<script src="javaScript/mobile-fisiotres.js"></script>
+	<script src="javaScript/jquery-3.4.1.min.js"></script>
+    <script src="javaScript/bootstrap-4.4.1.js"></script>
+
+
